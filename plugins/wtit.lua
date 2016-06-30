6e7a73edca1f064cf3fab82db2feb5ede3b403ef
telecerner <amircerner@gmail.com>
2016-06-30 17:17:51 +0430

    Update and rename wtt.lua to wtit.lua

--- plugins/wtit.lua
+++ plugins/wtit.lua
@@ -186,7 +186,7 @@ local function run(msg, matches)
 		a=a+1
 		result_text = result_text..a.."- "..result[a].."\n\n"
 	end
-	return result_text.."@Taylor_Team"
+	return result_text.."antispam-cerner"
 end
 
 return {
