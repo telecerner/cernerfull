ba04cd06bfa36ed52e4cf761676ac5a6fc35d7a3
telecerner <amircerner@gmail.com>
2016-06-30 17:16:07 +0430

    Update mean.lua

--- plugins/mean.lua
+++ plugins/mean.lua
@@ -1,7 +1,7 @@
 local function run(msg, matches)
   local htp = http.request('http://api.vajehyab.com/v2/public/?q='..URL.escape(matches[1]))
   local data = json:decode(htp)
-	return 'کلمه : '..(data.data.title or data.search.q)..'\n\nمعنی : '..(data.data.text or '----' )..'\n\nمنبع : '..(data.data.source or '----' )..'\n\n'..(data.error.message or '')..'\n\n@taylor_team'
+	return 'کلمه : '..(data.data.title or data.search.q)..'\n\nمعنی : '..(data.data.text or '----' )..'\n\nمنبع : '..(data.data.source or '----' )..'\n\n'..(data.error.message or '')..'m'
 end
 return {
   patterns = {
