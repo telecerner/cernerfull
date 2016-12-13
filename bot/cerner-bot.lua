229f6a66354ca5cc47deda2d1da6ea769a2b340c
telecerner <amircerner@gmail.com>
2016-12-13 16:29:54 +0330

    Update cerner-bot.lua

--- bot/cerner-bot.lua
+++ bot/cerner-bot.lua
@@ -233,7 +233,7 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {299362763},--Sudo users    moderation = {data = 'data/moderation.json'},
+    sudo_users = {329467212},--Sudo users    moderation = {data = 'data/moderation.json'},
     help_text_realm = [[
 Realm Commands:
 
